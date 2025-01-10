e384fd9d3e262017b2876a8786637a68c76aa929
Leo Marquez <142362735+LJMarquez@users.noreply.github.com>
2025-01-10 00:28:42 +0000

    end of jan 9

---
+++ php_practice/hello.php
@@ -0,0 +1,8 @@
+<?php
+
+$myName = "Leo";
+$myFriend = "Chicken little";
+echo "Hello, World. \n";
+echo "Me llamo $myName \n";
+echo "Mi aming se llamo $myFriend \n";
+echo "Hello " . $myFriend . " how r u \n";
